50011ba14f8bf89546f1b686b98150e632a64ca5
Cybergen <41336298+Cybergen300@users.noreply.github.com>
2020-08-31 20:59:33 +0200

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import Navbar from './Navbar'
+import Main from './Main'
+
+
+class App extends Component {
+
+  render() {
+      return(
+      	<p> Hello World </p>
+      	);
+  }
+}
+
+export default App;
+//start to code here 
